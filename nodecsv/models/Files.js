6f65f09859e3c65c23dcab52fc2885c2f015bc0e
gmarin93 <arias.fex@gmail.com>
2021-03-14 23:28:29 -0600

    Prototype

---
+++ nodecsv/models/Files.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const filesSchema = new Schema({
+
+    uuid:{
+        type:String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true
+    },
+    vin :{
+        type:String,
+        lowercase: true,
+        trim:true
+    },
+    make :{
+        type:String,
+        lowercase: true,
+        trim:true
+    },
+    model :{
+        type:String,
+        lowercase: true,
+        trim:true
+    },
+    mileage :{
+        type:String,
+        lowercase: true,
+        trim:true
+    },
+    year :{
+        type:Number,
+        lowercase: true,
+        trim:true
+    },
+    price :{
+        type:Number,
+        lowercase: true,
+        trim:true
+    },
+    zipcode :{
+        type:Number,
+        lowercase: true,
+        trim:true
+    } 
+
+});
+
+module.exports = mongoose.model('Files',filesSchema);
---
+++ nodecsv/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const indexControler = require('../controllers/indexController');
+
+
+router.post('/',
+    indexControler.loadFile
+);
+
+
+module.exports = router;
